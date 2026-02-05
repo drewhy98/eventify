0e50d76123753aa08c6ddc233dcb82c354ebc543
drewhy98 <150239451+drewhy98@users.noreply.github.com>
2026-02-05 16:27:19 +0000

    Create index.php
    styling now in its own file

---
+++ site/index.php
@@ -0,0 +1,15 @@
+<?php
+include 'includes/header.php';
+?>
+
+<!-- MAIN FEATURED -->
+<section class="featured">
+    <h2>Featured This Month</h2>
+    <img
+        src="https://static.vecteezy.com/system/resources/previews/024/603/816/large_2x/rock-music-concert-background-illustration-ai-generative-free-photo.jpg"
+        alt="Concert">
+</section>
+
+<?php
+include 'includes/footer.php';
+?>
